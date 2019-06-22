92e03afc7ee4fef6bb225afa9abe5afff47a1491
Mary Selig <seligmar@gmail.com>
2019-06-22 13:06:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -33,8 +33,9 @@ def adding
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+  shipping_manifest["muskets"] += 2 
+  shipping_manifest["gun powder"] += 4
 
   # return the shipping_manifest hash below
- # return shipping_manifest
+ return shipping_manifest
 end
